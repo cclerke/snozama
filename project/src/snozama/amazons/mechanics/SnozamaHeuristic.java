05cc76745b62cdc907d2bfda376d8889ffff8b31
Cody Clerke <codyclerke@live.ca>
2013-01-22 23:06:44 -0800

    Add board idea, types for empty, white, black, arrow

--- project/src/snozama/amazons/mechanics/SnozamaHeuristic.java
+++ project/src/snozama/amazons/mechanics/SnozamaHeuristic.java
@@ -16,6 +16,20 @@ public class SnozamaHeuristic {
 	 * 	This is suggested later when owning squares becomes more important
 	 */
 	
+	//board and player/arrow idea: byte is the smallest type to store this (that I'm aware of)
+	//temporary location, not sure where it will go just yet
+	/*
+	 * Starting setup
+	 *  -WHITE at [6][0], [9][3], [9][6], [6][9]
+	 *  -BLACK at [3][0], [0][3], [0][6], [3][9]
+	 *  -the rest will be empty
+	 */
+	byte board[][] = new byte[10][10];
+	public static byte EMPTY = 0;
+	public static byte WHITE = 1;
+	public static byte BLACK = 2;
+	public static byte ARROW = 3;
+	
 	/**
 	* Calculates closest player to each open square on the board
 	* The closest player to a square owns that square
