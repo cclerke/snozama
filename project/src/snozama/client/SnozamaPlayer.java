3ee72bb9ab388a76db3fa0e2370da13302f473d8
Cody Clerke <codyclerke@live.ca>
2013-01-23 22:18:56 -0800

    Add join room function, message handling for join room

--- project/src/snozama/client/SnozamaPlayer.java
+++ project/src/snozama/client/SnozamaPlayer.java
@@ -1,9 +1,13 @@
 package snozama.client;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Iterator;
 
+import net.n3.nanoxml.IXMLElement;
+
 import ubco.ai.GameRoom;
+import ubco.ai.connection.ServerMessage;
 import ubco.ai.games.GameClient;
 import ubco.ai.games.GameMessage;
 import ubco.ai.games.GamePlayer;
@@ -17,6 +21,9 @@ import ubco.ai.games.GamePlayer;
 public class SnozamaPlayer implements GamePlayer
 {
 	private GameClient gameClient;
+	private GameRoom room;
+	
+	private boolean isWhite = false;
 	
 	/**
 	 * Constructor where player name and password may be arbitrarily set.
@@ -46,11 +53,11 @@ public class SnozamaPlayer implements GamePlayer
 	
 	/**
 	 * Handle a game message in String form.
-	 * @param arg0		String object containing message to handle.
+	 * @param msg		String object containing message to handle.
 	 * @return @value true if message handled successfully, @value false otherwise.
 	 */
 	@Override
-	public boolean handleMessage(String arg0) throws Exception
+	public boolean handleMessage(String msg) throws Exception
 	{
 		// TODO Auto-generated method stub
 		return false;
@@ -58,14 +65,46 @@ public class SnozamaPlayer implements GamePlayer
 
 	/**
 	 * Handle a game message from GameMessage object.
-	 * @param arg0		GameMessage object containing message to handle.
+	 * @param msg		GameMessage object containing message to handle.
 	 * @return @value true if message handled successfully, @value false otherwise.
 	 */
 	@Override
-	public boolean handleMessage(GameMessage arg0) throws Exception
+	public boolean handleMessage(GameMessage msg) throws Exception
 	{
-		// TODO Auto-generated method stub
+		IXMLElement xml = ServerMessage.parseMessage(msg.msg);
+		String type = xml.getAttribute("type", "ERROR!");
+		System.out.println(msg);
+		
+		if (type.equals(GameMessage.ACTION_ROOM_JOINED))
+		{
+			onJoinRoom(xml);
+		}
+		else if (type.equals(GameMessage.ACTION_GAME_START))
+		{
+			//TODO Handle GameStart message
+		}
+		else if (type.equals(GameMessage.ACTION_MOVE))
+		{
+			//TODO Handle MoveAction message
+		}
+		
 		return false;
+	}
+	
+	//onJoinRoom function written by Yong
+	//what does this do? these variable aren't used anywhere
+	private void onJoinRoom(IXMLElement xml)
+	{
+		IXMLElement users = xml.getFirstChildNamed("usrlist");
+		int userCount = users.getAttribute("ucount", -1);
+		
+		Enumeration<?> children = users.enumerateChildren();
+		while (children.hasMoreElements())
+		{
+			IXMLElement user = (IXMLElement)children.nextElement();
+			int id = user.getAttribute("id", -1);
+			String name = user.getAttribute("name", "snozama"); //second value is default, maybe an error message instead
+		}
 	}
 	
 	/**
@@ -81,6 +120,17 @@ public class SnozamaPlayer implements GamePlayer
 			System.out.println("Game Room " + next.roomID + ": " + next.roomName);
 			System.out.println("\t-Number of users: " + next.userCount);
 		}
+	}
+	
+	/**
+	 * Request to join a room
+	 * @param roomIndex	The index of the room the client will join (0-6)
+	 */
+	public void joinRoom(int roomId)
+	{
+		room = gameClient.roomList.get(roomId);
+		gameClient.joinGameRoom(room.roomName);
+		System.out.println("Joining "+room.roomName+"...");
 	}
 	
 }
--- project/src/snozama/runtime/demos/SnozamaPlayerDemo.java
+++ project/src/snozama/runtime/demos/SnozamaPlayerDemo.java
@@ -7,6 +7,7 @@ public class SnozamaPlayerDemo
 	public static void main(String args[])
 	{
 		SnozamaPlayer player = new SnozamaPlayer("snozama", "alex2cody7!graeme");
-		player.printRooms();
+		player.joinRoom(0);
+		//player.printRooms();
 	}
 }
