68fc04b63445bbb31d8aa43b06bc7df2e06ea51a
Graeme Douglas <gdouglas.dev@gmail.com>
2013-02-01 08:11:12 -0800

    Actually add GlobalFunctions

---
+++ project/src/snozama/amazons/global/GlobalFunctions.java
@@ -0,0 +1,40 @@
+package snozama.amazons.global;
+
+public class GlobalFunctions {
+	/**
+	 * Function that flips input from 1 to 0 or 0 to 1.
+	 * 
+	 * @param input		The integer whose value one wants to flip.
+	 * @return	{@value 1} if input is {@value 0}, {@value 1} otherwise.
+	 */
+	public static int flip(int input)
+	{
+		if (input == 0)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	/**
+	 * Returns max of the input.
+	 * 
+	 * @param a		An integer to be compared.
+	 * @param b		An integer to be compared.
+	 * @return	The maximum of {@value a} and {@value b}.
+	 */
+	public static int max(int a, int b)
+	{
+		if (b > a)
+		{
+			return b;
+		}
+		else
+		{
+			return a;
+		}
+	}
+}
---
+++ project/src/snozama/amazons/global/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing globally used functionality. See contents for specific
+ * documentation.
+ */
+package snozama.amazons.global;
