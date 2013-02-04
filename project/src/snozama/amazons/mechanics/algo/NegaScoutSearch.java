4c17807df8d103cea1b73b9b86ff3c0172cde94e
Graeme Douglas <gdouglas.dev@gmail.com>
2013-02-03 19:11:14 -0800

    Debug NegaScoutSearch

--- project/src/snozama/amazons/mechanics/algo/NegaScoutSearch.java
+++ project/src/snozama/amazons/mechanics/algo/NegaScoutSearch.java
@@ -15,6 +15,8 @@ import ubco.ai.games.GameTimer;
  */
 public class NegaScoutSearch implements MoveChoiceAlgorithm
 {
+	static final int debug_limit = Integer.MAX_VALUE;
+	//static final int debug_limit = 5;
 	/**
 	 * Choose next move using NegaScout algorithm.
 	 * @param board		The board from which the decision is to be made.
@@ -29,23 +31,27 @@ public class NegaScoutSearch implements MoveChoiceAlgorithm
 		int best = -1;
 		int next = 0;
 		int bestScore = Integer.MIN_VALUE;
-		int currentScore = 0;
+		int beta = Integer.MAX_VALUE;
+		int currentScore = Integer.MIN_VALUE;
 		MoveManager successors = board.getSuccessors(colour, turn);
 		
-		while (successors.hasIterations()) // TODO: Still have time left, other constraints;
+		while (successors.hasIterations() && next < debug_limit) // TODO: Still have time left, other constraints;
 		{
+			System.out.println(next);
 			next = successors.nextIterableIndex();
 			
 			int arr_s = Board.decodeAmazonRow(board.amazons[colour][successors.getAmazonIndex(next)]);
 			int col_s = Board.decodeAmazonColumn(board.amazons[colour][successors.getAmazonIndex(next)]);
+			
 			successors.applyMove(board, next);
 			
-			currentScore = 1*recursiveNegaScout(board, colour, turn, Integer.MIN_VALUE, Integer.MAX_VALUE, 1, 1);
+			currentScore = 1*recursiveNegaScout(board, colour, turn, currentScore, beta, 1, 3);
 			
 			if (currentScore > bestScore)
 			{
 				best = next;
 				bestScore = currentScore;
+				beta = currentScore+1;
 			}
 			
 			successors.undoMove(board, next, arr_s, col_s);
@@ -64,7 +70,7 @@ public class NegaScoutSearch implements MoveChoiceAlgorithm
 	 * @param beta		The upper bound of the expected value of the subtree.x
 	 * @param depth		The depth of the current node from the initial search node.
 	 * @param cutoff	The depth from the initial search node at which to go no deeper.
-	 * @return
+	 * @return	Return estimated score for a given board.
 	 */
 	public static int recursiveNegaScout(Board board, int colour, int turn, int alpha, int beta, int depth, int cutoff)
 	{
@@ -80,9 +86,11 @@ public class NegaScoutSearch implements MoveChoiceAlgorithm
 		
 		int b = beta;
 		
+		colour = GlobalFunctions.flip(colour);
+		
 		MoveManager successors = board.getSuccessors(colour, turn);
 		
-		while (successors.hasIterations())
+		while (successors.hasIterations() && next < debug_limit)
 		{
 			next = successors.nextIterableIndex();
 			
@@ -91,13 +99,13 @@ public class NegaScoutSearch implements MoveChoiceAlgorithm
 			
 			successors.applyMove(board, next);
 			
-			int score = -1*recursiveNegaScout(board, GlobalFunctions.flip(colour), turn+1, -1*b, -1*alpha, depth+1, cutoff);
+			int score = -1*recursiveNegaScout(board, colour, turn+1, -1*b, -1*alpha, depth+1, cutoff);
 			
 			// If we fail high and this is not the first child node processed
 			if (alpha < score && score < beta && processed != 0)
 			{
 				// Full re-search.
-				score = -1*recursiveNegaScout(board, GlobalFunctions.flip(colour), turn+1, -1*beta, -1*alpha, depth+1, cutoff);
+				score = -1*recursiveNegaScout(board, colour, turn+1, -1*beta, -1*alpha, depth+1, cutoff);
 			}
 			
 			alpha = GlobalFunctions.max(score, alpha);
