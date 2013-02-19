8159f2c0596b9489a799fba14e479c07233a2759
Graeme Douglas <gdouglas.dev@gmail.com>
2013-02-18 19:10:46 -0800

    Complete and test ZobristTTable.java, should be ready to roll
    Only thing I worry about is overhead of managing array of arrays for
    hashtable.  I'd like to just make it a single dimension.  Also not happy
    about "key % size" everywhere, but what do you do?  It would be cool to
    get collision rate to 0 in every case.

--- project/src/snozama/amazons/mechanics/transtable/ZobristTTable.java
+++ project/src/snozama/amazons/mechanics/transtable/ZobristTTable.java
@@ -2,6 +2,8 @@ package snozama.amazons.mechanics.transtable;
 
 import java.util.Random;
 
+import snozama.amazons.mechanics.Board;
+
 /**
  * A transposition table using Zobrist hashing.
  * 
@@ -11,58 +13,296 @@ import java.util.Random;
 public class ZobristTTable implements TranspositionTable
 {
 	/**
-	 * Arrow index for Zobrist hash.
+	 * White amazon index for Zobrist values.
 	 */
-	private static final int ARROW = 0;
+	public static final int WHITE_AMAZON = 0;
 	
 	/**
-	 * White amazon index for Zobrist hash.
+	 * Black amazon index for Zobrist values.
 	 */
-	private static final int WHITE_AMAZON = 1;
+	public static final int BLACK_AMAZON = 1;
 	
 	/**
-	 * Black amazon index for Zobrist hash.
+	 * Arrow index for Zobrist values.
 	 */
-	private static final int BLACK_AMAZON = 2;
+	public static final int ARROW = 2;
 	
-	// TODO: Can we make this general to all ZobristTTables?  Does it matter?
+	/**
+	 * Size of the hash table.
+	 */
+	public final int size;
+	
+	/**
+	 * The position of the score within the stored record.
+	 */
+	public static final int SCORE = 0;
+	
+	/**
+	 * The position of the depth within the stored record.
+	 */
+	public static final int DEPTH = 1;
+	
+	/**
+	 * The position of the lower bound within the stored record.
+	 */
+	public static final int LOWER = 2;
+	
+	/**
+	 * The position of the upper bound within the stored record.
+	 */
+	public static final int UPPER = 3;
+	
+	/**
+	 * The position of the move within the stored record.
+	 */
+	public static final int MOVE  = 4;
+	
+	/**
+	 * The number of collisions that have occurred so far.
+	 */
+	public int collisions;
+	
+	// TODO: Can we make this general to all ZobristTTables?  Does it matter? (should it be static)
 	/**
 	 * Table storing all possible Zobrist values.
 	 */
-	private final long[][][] zobristValues;
+	private final int[][][] zobristValues;
 	
-	// TODO: Decide record format.  Long _should_ be big enough.
 	/**
 	 * The hash table.  The key is the result of the Zobrist hash; the record
 	 * is a long with all the relevant information.
 	 */
-	public long[] hashTable;
+	public int[][] hashTable;
 	
 	/**
 	 * Default constructor for the transposition table.
 	 */
-	public ZobristTTable()
+	public ZobristTTable(int size)
 	{
-		zobristValues = new long[3][10][10];
+		zobristValues = new int[3][10][10];
 		
+		// 
 		Random rand = new Random();
+		//for (int i = 0; i < 10000; i++)
+		//	rand.nextInt();
 		
 		// Create the Zobrist value lookup.
 		for (int t = 0; t < zobristValues.length; t++)
 		{
 			for (int i = 0; i < zobristValues[t].length; i++)
 			{
-				for (int j = 0; i < zobristValues[t][i].length; j++)
+				for (int j = 0; j < zobristValues[t][i].length; j++)
 				{
-					zobristValues[t][i][j] = rand.nextLong();
+					zobristValues[t][i][j] = rand.nextInt(Integer.MAX_VALUE);
 				}
 			}
 		}
 		
-		// TODO: What is the correct size?
-		hashTable = new long[10000000];
+		this.size = size;
+		
+		collisions = 0;
+		
+		hashTable = new int[size][5];
+		for (int i = 0; i < hashTable.length; i++)
+		{
+			hashTable[i][DEPTH] = -1;
+		}
 	}
 	
+	/**
+	 * Computes the hash of the board.
+	 * 
+	 * @param board		The board compute the hash of.
+	 * @return			The computed zobrist hash of the board.
+	 */
+	public int computeBoardHash(Board board)
+	{
+		int hashed = 0;
+		
+		int i = 0;
+		int j = 0;
+		
+		// Find first value that needs to be hashed.
+		for (; i < 10; i++)
+		{
+			for (; j < 10; j++)
+			{
+				if (!board.isOccupied(i, j))
+					continue;
+				
+				
+				if (board.isWhite(i, j))
+				{
+					hashed = zobristValues[WHITE_AMAZON][i][j];
+				}
+				else if (board.isBlack(i, j))
+				{
+					hashed = zobristValues[BLACK_AMAZON][i][j];
+				}
+				else
+				{
+					hashed = zobristValues[ARROW][i][j];
+				}
+				
+				if (hashed != 0)
+				{
+					break;
+				}
+			}
+			
+			if (hashed != 0)
+			{
+				j++;
+				break;
+			}
+		}
+		
+		// Proceed as normal.
+		for (; i < 10; i++)
+		{
+			for (; j < 10; j++)
+			{
+				if (board.isWhite(i, j))
+				{
+					hashed ^= zobristValues[WHITE_AMAZON][i][j];
+				}
+				else if (board.isBlack(i, j))
+				{
+					hashed ^= zobristValues[BLACK_AMAZON][i][j];
+				}
+				else if (board.isOccupied(i, j))
+				{
+					hashed ^= zobristValues[ARROW][i][j];
+				}
+			}
+			j = 0;
+		}
+		
+		return hashed;
+	}
 	
+	/**
+	 * Update a generated key based on move values. Can be used to apply
+	 * or undo a move. Note that the garbage in, garbage out principle
+	 * applies.
+	 * 
+	 * @param key		The key to be updated.
+	 * @param colour	The player colour (note, should use static constants
+	 * 					from this class)
+	 * @param row_s		The row that the moved amazon started from.
+	 * @param col_s		The column that the moved amazon started from.
+	 * @param row_f		The row that the moved amazon finished at.
+	 * @param col_f		The column that the moved amazon finished at.
+	 * @param arow		The row the arrow was placed.
+	 * @param acol		The column the arrow was placed in.
+	 * @return			The updated key.
+	 */
+	public int updateHashKeyByMove(int key, int colour, int row_s, int col_s, int row_f, int col_f, int arow, int acol)
+	{
+		// General principle here: garbage in, garbage out.  Don't make bad calls.
+		key ^= zobristValues[colour][row_s][col_s];
+		key ^= zobristValues[colour][row_f][col_f];
+		key ^= zobristValues[ARROW][arow][acol];
+		
+		return key;
+	}
 	
+	/**
+	 * Add entry to transposition table.
+	 * 
+	 * @param board		The board (position) that is to be hashed.
+	 * @param score		The score of the board to be hashed.
+	 * @param depth		The depth of the board to be hashed.
+	 * @param lower		The lower bound used to find the score of the hashed
+	 * 					board.
+	 * @param upper		The upper bound used to find the value of the hashed
+	 * 					board.
+	 * @param move		The {@code MoveManager} encoded move data.
+	 * @return			{@code true} if the value was added properly,
+	 * 					{@code false} otherwise.
+	 */
+	public boolean put(Board board, int score, int depth, int lower, int upper, int move)
+	{
+		int key = computeBoardHash(board) % size;
+		
+		// For now, we will use the "newest" replacement strategy.
+		if (get(key)[DEPTH] != -1)
+		{
+			collisions++;
+		}
+		
+		hashTable[key][SCORE] = score;
+		hashTable[key][DEPTH] = depth;
+		hashTable[key][LOWER] = lower;
+		hashTable[key][UPPER] = upper;
+		hashTable[key][MOVE]  = move;
+		
+		return true;
+	}
+	
+	/**
+	 * Add entry to transposition table.
+	 * 
+	 * @param key		The key of the location to place the value in.
+	 * @param score		The score of the board to be hashed.
+	 * @param depth		The depth of the board to be hashed.
+	 * @param lower		The lower bound used to find the score of the hashed
+	 * 					board.
+	 * @param upper		The upper bound used to find the value of the hashed
+	 * 					board.
+	 * @param move		The {@code MoveManager} encoded move data.
+	 * @return			{@code true} if the value was added properly,
+	 * 					{@code false} otherwise.
+	 */
+	public boolean put(int key, int score, int depth, int lower, int upper, int move)
+	{
+		key %= size;
+		// For now, we will use the "newest" replacement strategy.
+		if (get(key)[DEPTH] != -1)
+		{
+			collisions++;
+		}
+		
+		hashTable[key][SCORE] = score;
+		hashTable[key][DEPTH] = depth;
+		hashTable[key][LOWER] = lower;
+		hashTable[key][UPPER] = upper;
+		hashTable[key][MOVE]  = move;
+		
+		return true;
+	}
+	
+	/**
+	 * Get an entry from the transposition table, based on hashed key.
+	 * 
+	 * @param key		The key of the entry to retrieve.
+	 * @return			The record of values stored.
+	 */
+	public int[] get(int key)
+	{
+		key %= size;
+		return hashTable[key];
+	}
+	
+	/**
+	 * Get an entry from the transposition table, based on board to be hashed.
+	 * 
+	 * @param board		The board that determines the hash key.
+	 * @return			The record of values stored.
+	 */
+	public int[] get(Board board)
+	{
+		int key = computeBoardHash(board) % size;
+		
+		return hashTable[key];
+	}
+	
+	/**
+	 * Get the number of entries of the transposition table.
+	 * @return
+	 */
+	public int size()
+	{
+		return this.hashTable[0].length;
+	}
 }
---
+++ project/src/snozama/runtime/tests/mechanics/ZobristTTableTest.java
@@ -0,0 +1,131 @@
+package snozama.runtime.tests.mechanics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import snozama.amazons.mechanics.Board;
+import snozama.amazons.mechanics.MoveManager;
+import snozama.amazons.mechanics.SnozamaHeuristic;
+import snozama.amazons.mechanics.transtable.ZobristTTable;
+
+public class ZobristTTableTest {
+
+	@Test
+	public void testNew() {
+		ZobristTTable table = new ZobristTTable(1);
+		assertEquals(table.size, 1);
+		
+		table = new ZobristTTable(1000);
+		assertEquals(table.size, 1000);
+	}
+	
+	@Test
+	public void testPutGetBasic() {
+		ZobristTTable table = new ZobristTTable(2500000);
+		
+		Board board = new Board();
+		
+		// Totally fake data.
+		table.put(board, 10, 0, -100, -10, 233);
+		
+		// Get the data.
+		int[] record = table.get(board);
+		
+		// Test data.
+		assertEquals(record[ZobristTTable.SCORE], 10);
+		assertEquals(record[ZobristTTable.DEPTH], 0);
+		assertEquals(record[ZobristTTable.LOWER], -100);
+		assertEquals(record[ZobristTTable.UPPER], -10);
+		assertEquals(record[ZobristTTable.MOVE], 233);
+		
+		record = null;
+		
+		record = table.get(table.computeBoardHash(board));
+		assertEquals(record[ZobristTTable.SCORE], 10);
+		assertEquals(record[ZobristTTable.DEPTH], 0);
+		assertEquals(record[ZobristTTable.LOWER], -100);
+		assertEquals(record[ZobristTTable.UPPER], -10);
+		assertEquals(record[ZobristTTable.MOVE], 233);
+	}
+	
+	// Note, get cannot be tested well, because there might be a collision we cannot predict.
+	@Test
+	public void testPutMany()
+	{
+		ZobristTTable table = new ZobristTTable(2500000);
+		
+		Board board = new Board();
+		MoveManager successors = board.getSuccessors(Board.WHITE);
+		int next;
+		int score;
+		int row_s;
+		int col_s;
+		
+		while (successors.hasIterations())
+		{
+			next = successors.nextIterableIndex();
+			row_s = Board.decodeAmazonRow(board.amazons[Board.WHITE][successors.getAmazonIndex(next)]);
+			col_s = Board.decodeAmazonColumn(board.amazons[Board.WHITE][successors.getAmazonIndex(next)]);
+			successors.applyMove(board, next);
+			
+			score = SnozamaHeuristic.evaluateBoard(board, Board.WHITE, 1);
+			
+			// Some fakery here.
+			table.put(board, score, 1, -100, 100, successors.getMove(next));
+			
+			successors.undoMove(board, next, row_s, col_s);
+		}
+		
+		System.out.printf("Total number of collisions: %d\n", table.collisions);
+	}
+	
+	// Note, get cannot be tested well, because there might be a collision we cannot predict.
+	@Test
+	public void testPutManyUpdateKey()
+	{
+		ZobristTTable table = new ZobristTTable(2500000);
+
+		Board board = new Board();
+		int key = table.computeBoardHash(board);
+		int baseKey = key;
+		MoveManager successors = board.getSuccessors(Board.WHITE);
+		int next;
+		int score;
+		int row_s;
+		int col_s;
+
+		while (successors.hasIterations())
+		{
+			next = successors.nextIterableIndex();
+			row_s = Board.decodeAmazonRow(board.amazons[Board.WHITE][successors.getAmazonIndex(next)]);
+			col_s = Board.decodeAmazonColumn(board.amazons[Board.WHITE][successors.getAmazonIndex(next)]);
+			successors.applyMove(board, next);
+			
+			// Make move to key.
+			key = table.updateHashKeyByMove(key, ZobristTTable.WHITE_AMAZON,
+					row_s, col_s, successors.getFinishRow(next),
+					successors.getFinishColumn(next),
+					successors.getArrowRow(next),
+					successors.getArrowColumn(next));
+			
+			score = SnozamaHeuristic.evaluateBoard(board, Board.WHITE, 1);
+
+			// Some fakery here.
+			table.put(key, score, 1, -100, 100, successors.getMove(next));
+
+			successors.undoMove(board, next, row_s, col_s);
+			
+			// Undo move to key
+			key = table.updateHashKeyByMove(key, ZobristTTable.WHITE_AMAZON,
+					row_s, col_s, successors.getFinishRow(next),
+					successors.getFinishColumn(next),
+					successors.getArrowRow(next),
+					successors.getArrowColumn(next));
+			
+			assertEquals(key, baseKey);
+		}
+
+		System.out.printf("Total number of collisions: %d\n", table.collisions);
+	}
+}
