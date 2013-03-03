91f36a105c72fee8e8adfdce010f61934bbf261b
Graeme Douglas <gdouglas.dev@gmail.com>
2013-03-02 20:50:48 -0800

    Fix previous push.. :)

--- project/src/snozama/runtime/demos/DualSnozamaPlayerDemo.java
+++ project/src/snozama/runtime/demos/DualSnozamaPlayerDemo.java
@@ -4,6 +4,7 @@ import snozama.amazons.global.GlobalFunctions;
 import snozama.amazons.mechanics.Board;
 import snozama.amazons.mechanics.MoveChoice;
 import snozama.amazons.mechanics.MoveManager;
+import snozama.amazons.mechanics.algo.NegaScout;
 import snozama.amazons.mechanics.algo.TranspositionNegaScout;
 import snozama.client.SnozamaPlayer;
 import snozama.ui.api.AUI;
@@ -33,14 +34,18 @@ public class DualSnozamaPlayerDemo
 		int turn = 1;
 		int move = 0;
 		int colour = Board.WHITE;
-		long decisionTime = 3000;	// In milliseconds.
+		long decisionTime = 1000;	// In milliseconds.
 		MoveChoice choice = null;
 		
 		do
 		{
-			TranspositionNegaScout search = new TranspositionNegaScout(System.currentTimeMillis()+decisionTime, 2000000, board);
-			move = search.chooseMove(board, Board.WHITE, turn);
-			MoveManager.applyUnmanagedMove(board, move);
+			NegaScout search = new NegaScout(System.currentTimeMillis()+decisionTime);
+			move = search.chooseMove(board, colour, turn);
+			if (move <= 0)
+			{
+				AUI.post("Game over.");
+				break;
+			}
 			choice = new MoveChoice(move, board);
 			try {
 				AUI.moveAmazon(choice.getStartingRow(),
@@ -53,6 +58,7 @@ public class DualSnozamaPlayerDemo
 				e.printStackTrace();
 				System.exit(-1);
 			}
+			MoveManager.applyUnmanagedMove(board, move);
 			
 			turn++;
 			colour = GlobalFunctions.flip(colour);
